632099647eb703a1f7844a575badf90da4fce15d
jfaldanam <me@jfaldanam.xyz>
2023-09-12 15:11:41 +0200

    feat: add location to coordinates function

--- src/eidos/functions/core.py
+++ src/eidos/functions/core.py
@@ -1,3 +1,9 @@
+import requests
+
+from eidos.logs import get_logger
+
+logger = get_logger()
+
 def salute(who: str) -> str:
     """Salute someone.
     
@@ -8,3 +14,30 @@ def salute(who: str) -> str:
         str: The salutation message.
     """
     return f"Hello, {who}! o7"
+
+
+def get_coordinates_by_location(location: str) -> tuple[float, float]:
+    """Get the coordinates of a location using OpenStreetMap.
+
+    Args:
+        location (str): The location to get the coordinates of.
+
+    Returns:
+        tuple[float, float]: The latitude and longitude of the location.
+    """
+
+    endpoint = "https://nominatim.openstreetmap.org/search"
+    params = {
+        'q': location,
+        'format': 'json'
+    }
+
+    response = requests.get(endpoint, params=params)
+    data = response.json()
+
+    if data:
+        lat, lon = float(data[0]['lat']), float(data[0]['lon'])
+        return lat, lon
+    else:
+        logger.info(f"Location not found: {location}")
+        return None
