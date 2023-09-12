4cd22f8558051d3700a6246e33868f372c5c91ba
jfaldanam <me@jfaldanam.xyz>
2023-09-12 13:10:35 +0200

    feat: implement reference function

---
+++ src/eidos/functions/core.py
@@ -0,0 +1,10 @@
+def salute(who: str) -> str:
+    """Salute someone.
+    
+    Args:
+        who (str): The name of whom to salute. o7
+    
+    Returns:
+        str: The salutation message.
+    """
+    return f"Hello, {who}! o7"
