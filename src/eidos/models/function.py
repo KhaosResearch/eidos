b448d74eb8fced882133844848a667571b732d74
benhid <antoniobenitezhid@gmail.com>
2024-03-06 15:13:43 +0100

    feat: refactor load function

--- src/eidos/models/function.py
+++ src/eidos/models/function.py
@@ -1,64 +1,49 @@
 import builtins
 
 from pydantic import BaseModel, Field, create_model
-from pydantic_core import PydanticUndefined
 
+from eidos.validation.type import parse_type_name
 from eidos.models.parameter import AiParameter
-from eidos.validation.type import split_type_from_generic
 
 
-def load_model(function_: dict) -> BaseModel:
-    """Load the pydantic model of a function from its JSON definition.
+def load_model(fn: dict) -> BaseModel:
+    """Load a Pydantic model from a function's JSON definition.
 
-    This pydantic model is compatible with OpenAI functions if exported as JSON schema.
+    This function creates a Pydantic model dynamically based on a provided JSON schema
+    representation of a function's parameters.
 
     Example:
-    ```
-    function_model = load_model(function)
-    function_model.model_json_schema()
-    ```
+    >> function_model = load_model(function)
+    >> function_model.model_json_schema()
 
     Args:
-        function_ (dict): JSON definition of the function.
+        fn (dict): JSON definition of the function.
 
     Returns:
-        BaseModel: Pydantic model of the function.
+        BaseModel: A dynamically created Pydantic model.
     """
     parameters = [
-        AiParameter.model_validate(parameter) for parameter in function_["parameters"]
+        AiParameter.model_validate(param) for param in fn.get("parameters", [])
     ]
-
     parameters_dict = {}
-    for v in parameters:
-        json_schema_extra = {}
-        if v.options:
-            json_schema_extra["enum"] = v.options
+    for param in parameters:
+        json_schema_extra = {"enum": param.options} if param.options else {}
 
+        # Resolve a type string to a Python type object.
+        # Note: OpenAI does not support custom generic types, so we only need to handle built-in types.
         type_ = getattr(
             builtins,
-            split_type_from_generic(v.type)[
-                0
-            ],  # OpenAI does not support generic types, remove it
+            parse_type_name(param.type)[0],
         )
 
-        # PydanticUndefined is used to mark parameters as not required
-        default_ = PydanticUndefined
-        if not v.required:
-            default_ = v.default
-
-        parameters_dict[v.name] = (
+        parameters_dict[param.name] = (
             type_,
             Field(
-                default=default_,  # Set a default value to mark them as not required
-                description=v.description,
-                pattern=v.regex,
+                default=(... if param.required else param.default),
+                description=param.description,
+                pattern=param.regex,
                 json_schema_extra=json_schema_extra,
             ),
         )
 
-    ai_function = create_model(
-        function_["name"],
-        **parameters_dict,
-    )
-
-    return ai_function
+    return create_model(fn["name"], **parameters_dict)
